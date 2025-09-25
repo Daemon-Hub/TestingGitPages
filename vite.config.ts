c230688661ca7143efc4bbd4c7032369d5581474
Daemon-Hub <islamkertov@gmail.com>
2025-09-25 23:22:30 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: 'docs'  // Измените 'dist' на 'docs'
+	},
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
